cmangos/mangos-cata

commit 915910303cb3a60e442d692af6d2e046a70db72c

[c12643] Correct logic when setting SPELL_FLAG_REDIRECTED

src/game/Spell.cpp
@@ -1895,9 +1895,12 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
             {
                 if (Unit* pUnitTarget = m_caster->SelectMagnetTarget(m_targets.getUnitTarget(), this, effIndex))
                 {
-                    m_targets.setUnitTarget(pUnitTarget);
-                    m_spellFlags |= SPELL_FLAG_REDIRECTED;
-                    targetUnitMap.push_back(pUnitTarget);
+                    if (m_targets.getUnitTarget() != pUnitTarget)
+                    {
+                        m_targets.setUnitTarget(pUnitTarget);
+                        m_spellFlags |= SPELL_FLAG_REDIRECTED;
+                        targetUnitMap.push_back(pUnitTarget);
+                    }
                 }
             }
             else
@@ -2459,8 +2462,11 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
         {
             if (Unit* pUnitTarget = m_caster->SelectMagnetTarget(m_targets.getUnitTarget(), this, effIndex))
             {
-                m_targets.setUnitTarget(pUnitTarget);
-                m_spellFlags |= SPELL_FLAG_REDIRECTED;
+                if (m_targets.getUnitTarget() != pUnitTarget)
+                {
+                    m_targets.setUnitTarget(pUnitTarget);
+                    m_spellFlags |= SPELL_FLAG_REDIRECTED;
+                }
                 targetUnitMap.push_back(pUnitTarget);
             }
             break;

src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12642"
+ #define REVISION_NR "12643"
 #endif // __REVISION_NR_H__

commit dadacb852b12dea7103141e2ff01ed54e843a30e

[c12651] Fix spell broken in 915910303cb3a60e442d692af6d2e046a70db72c
Signed-off-by: Dramacydal <PulLumBerMal@gmail.com>

src/game/Spell.cpp
@@ -1895,12 +1895,11 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
             {
                 if (Unit* pUnitTarget = m_caster->SelectMagnetTarget(m_targets.getUnitTarget(), this, effIndex))
                 {
-                    if (m_targets.getUnitTarget() != pUnitTarget)
-                    {
-                        m_targets.setUnitTarget(pUnitTarget);
+                    if (m_targets.getUnitTarget() && m_targets.getUnitTarget() != pUnitTarget)
                         m_spellFlags |= SPELL_FLAG_REDIRECTED;
-                        targetUnitMap.push_back(pUnitTarget);
-                    }
+
+                    m_targets.setUnitTarget(pUnitTarget);
+                    targetUnitMap.push_back(pUnitTarget);
                 }
             }
             else
@@ -2462,11 +2461,10 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
         {
             if (Unit* pUnitTarget = m_caster->SelectMagnetTarget(m_targets.getUnitTarget(), this, effIndex))
             {
-                if (m_targets.getUnitTarget() != pUnitTarget)
-                {
-                    m_targets.setUnitTarget(pUnitTarget);
+                if (m_targets.getUnitTarget() && m_targets.getUnitTarget() != pUnitTarget)
                     m_spellFlags |= SPELL_FLAG_REDIRECTED;
-                }
+
+                m_targets.setUnitTarget(pUnitTarget);
                 targetUnitMap.push_back(pUnitTarget);
             }
             break;

src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "12650"
+ #define REVISION_NR "12651"
 #endif // __REVISION_NR_H__