095e1b8ec966bb99ad63a36ee1029febbd0144a9
Sidsukana <pgsilent@gmail.com>
2013-08-19 17:30:21 +0300

    [c12702] Fix take ammo for most ranged spells
    Fix spells like Arcane Shot not taking ammo while they should
    
    Close cmangos/mangos-tbc#19
    
    Signed-off-by: cala <calaftp@free.fr>
    Signed-off-by: Schmoozerd <schmoozerd@cmangos.net>
    
    (based on commit [12510] - c8b2a94)
    
    Signed-off-by: Dramacydal <PulLumBerMal@gmail.com>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3438,6 +3438,7 @@ void Spell::cast(bool skipCheck)
 
     TakePower();
     TakeReagents();                                         // we must remove reagents before HandleEffects to allow place crafted item in same slot
+    TakeAmmo();
 
     SendCastResult(castResult);
     SendSpellGo();                                          // we must send smsg_spell_go packet before m_castItem delete in TakeCastItem()...
@@ -4761,6 +4762,34 @@ SpellCastResult Spell::CheckOrTakeRunePower(bool take)
     }
 
     return SPELL_CAST_OK;
+}
+
+void Spell::TakeAmmo()
+{
+    // take ammo
+    if (m_attackType == RANGED_ATTACK && m_caster->GetTypeId() == TYPEID_PLAYER)
+    {
+        Item* pItem = ((Player*)m_caster)->GetWeaponForAttack(RANGED_ATTACK, true, false);
+
+        // wands don't have ammo
+        if (!pItem || pItem->GetProto()->SubClass == ITEM_SUBCLASS_WEAPON_WAND)
+            return;
+
+        if (pItem->GetProto()->InventoryType == INVTYPE_THROWN)
+        {
+            if (pItem->GetMaxStackCount() == 1)
+            {
+                // decrease durability for non-stackable throw weapon
+                ((Player*)m_caster)->DurabilityPointLossForEquipSlot(EQUIPMENT_SLOT_RANGED);
+            }
+            else
+            {
+                // decrease items amount for stackable throw weapon
+                uint32 count = 1;
+                ((Player*)m_caster)->DestroyItemCount(pItem, count, true);
+            }
+        }
+    }
 }
 
 void Spell::TakeReagents()
