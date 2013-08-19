8d3efd769a61ba72f30ff1098d8b4cb62462f546
Shauren <shauren.trinity@gmail.com>
2013-08-19 16:44:04 +0300

    [c12678] Improve alcohol handling
    Fixes client crash when using Barbershop Chair while drunk
    
    Also allow .mod drunk to modify the drunk state of the targeted player
    
    Signed-off-by: stfx <stfx@hotmail.de>
    
    (based on commit [12487] - 4ece3ed)
    
    Signed-off-by: Dramacydal <PulLumBerMal@gmail.com>

---
+++ sql/updates/c12678_01_characters_characters.sql
@@ -0,0 +1,5 @@
+ALTER TABLE character_db_version CHANGE COLUMN c12631_01_characters_corpse required_c12678_01_characters_characters bit;
+
+UPDATE characters SET drunk = (drunk / 256) & 0xFF;
+
+ALTER TABLE characters CHANGE drunk drunk tinyint(3) unsigned NOT NULL DEFAULT '0';
