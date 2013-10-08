664b163c90a7ee10c4c651a879f31ce2f6986a38
Schmoozerd <schmoozerd@scriptdev2.com>
2013-10-08 12:45:53 +0400

    [c12755] EventAI - Add more developing error output
    (based on commit [12554] - 8af1dcf)

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -9830,7 +9830,7 @@ void Spell::EffectLeapForward(SpellEffectEntry const* effect)
     if (unitTarget->IsTaxiFlying())
         return;
 
-    if (m_spellInfo->rangeIndex == 1)                       // self range
+    if (m_spellInfo->rangeIndex == SPELL_RANGE_IDX_SELF_ONLY)
     {
         float dis = GetSpellRadius(sSpellRadiusStore.LookupEntry(effect->GetRadiusIndex()));
 
