f157a3eed68bd3a07082744a27918dd88412a416
Xfurry <xfurry@scriptdev2.com>
2014-01-19 22:24:38 +0100

    [c12780] Implement spells for Mimiron encounter
    Spell effects: 64402, 65346, 63027, 63667, 63681, 64841, 63382
    Drop spells 63820, 64425, 64620, 64398, 64426, 64621 because they will be handled by script
    
    (based on commit [12574] - 5b4e851)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -7983,6 +7983,7 @@ void Spell::GetSpellRangeAndRadius(SpellEffectEntry const* spellEffect, float& r
                 case 63820:                                 // Summon Scrap Bot Trigger (Ulduar, Mimiron) use for Scrap Bots, hits npc 33856
                 case 64218:                                 // Overcharge (VoA, Emalon)
                 case 64234:                                 // Gravity Bomb (h) (Ulduar, XT-002)
+                case 64402:                                 // Rocket Strike (Ulduar, Mimiron)
                 case 64425:                                 // Summon Scrap Bot Trigger (Ulduar, Mimiron) use for Assault Bots, hits npc 33856
                 case 64531:                                 // Rapid Burst (h)
                 case 64543:                                 // Melt Ice (Ulduar, Hodir)
