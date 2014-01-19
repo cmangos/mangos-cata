0efe5625a8652e4da02ae9d587482e74a024147d
Cyberium <cybermomo@techemail.com>
2014-01-19 22:36:33 +0100

    [c12791] Rename m_respawnAggroDelay to make it more generic
    (based on commit [12584] - 8496e12)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -138,7 +138,7 @@ Creature::Creature(CreatureSubtype subtype) : Unit(),
     lootForPickPocketed(false), lootForBody(false), lootForSkin(false),
     m_groupLootTimer(0), m_groupLootId(0),
     m_lootMoney(0), m_lootGroupRecipientId(0),
-    m_corpseDecayTimer(0), m_respawnTime(0), m_respawnDelay(25), m_corpseDelay(60), m_respawnradius(5.0f), m_respawnAggroDelay(0),
+    m_corpseDecayTimer(0), m_respawnTime(0), m_respawnDelay(25), m_corpseDelay(60), m_respawnradius(5.0f), m_aggroDelay(0),
     m_subtype(subtype), m_defaultMovementType(IDLE_MOTION_TYPE), m_equipmentId(0),
     m_AlreadyCallAssistance(false), m_AlreadySearchedAssistance(false),
     m_regenHealth(true), m_AI_locked(false), m_isDeadByDefault(false),
@@ -476,7 +476,7 @@ void Creature::Update(uint32 update_diff, uint32 diff)
             {
                 DEBUG_FILTER_LOG(LOG_FILTER_AI_AND_MOVEGENSS, "Respawning...");
                 m_respawnTime = 0;
-                m_respawnAggroDelay = sWorld.getConfig(CONFIG_UINT32_CREATURE_RESPAWN_AGGRO_DELAY);
+                m_aggroDelay = sWorld.getConfig(CONFIG_UINT32_CREATURE_RESPAWN_AGGRO_DELAY);
                 lootForPickPocketed = false;
                 lootForBody         = false;
                 lootForSkin         = false;
@@ -544,10 +544,10 @@ void Creature::Update(uint32 update_diff, uint32 diff)
         }
         case ALIVE:
         {
-            if (m_respawnAggroDelay <= update_diff)
-                m_respawnAggroDelay = 0;
+            if (m_aggroDelay <= update_diff)
+                m_aggroDelay = 0;
             else
-                m_respawnAggroDelay -= update_diff;
+                m_aggroDelay -= update_diff;
 
             if (m_isDeadByDefault)
             {
@@ -1850,7 +1850,7 @@ bool Creature::CanInitiateAttack()
     if (isPassiveToHostile())
         return false;
 
-    if (m_respawnAggroDelay != 0)
+    if (m_aggroDelay != 0)
         return false;
 
     return true;
