198f4d7a0b466c6761c08e111076497a701a302e
Schmoozerd <schmoozerd@cmangos.net>
2015-02-27 23:41:44 +0100

    [c13018] Improve Weather handling
    * Move Weather handling from World to Map class
    * Add new Map::SetWeather function to force a weather for a zone
    * Allow .wchange command to also create Thunderstorm or BlackRain
    
    (based on commit [12807] - ca4f8c3)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/ObjectMgr.h
+++ src/game/ObjectMgr.h
@@ -331,19 +331,6 @@ struct QuestPhaseMaps
 typedef std::vector<QuestPhaseMaps> QuestPhaseMapsVector;
 typedef UNORDERED_MAP<uint32, QuestPhaseMapsVector> QuestPhaseMapsMap;
 
-#define WEATHER_SEASONS 4
-struct WeatherSeasonChances
-{
-    uint32 rainChance;
-    uint32 snowChance;
-    uint32 stormChance;
-};
-
-struct WeatherZoneChances
-{
-    WeatherSeasonChances data[WEATHER_SEASONS];
-};
-
 struct DungeonEncounter
 {
     DungeonEncounter(DungeonEncounterEntry const* _dbcEntry, EncounterCreditType _creditType, uint32 _creditEntry, uint32 _lastEncounterDungeon)
@@ -544,7 +531,6 @@ class ObjectMgr
 
         typedef UNORDERED_MAP<uint32, PointOfInterest> PointOfInterestMap;
 
-        typedef UNORDERED_MAP<uint32, WeatherZoneChances> WeatherZoneMap;
 
         void LoadGameobjectInfo();
         void AddGameobjectInfo(GameObjectInfo* goinfo);
@@ -770,7 +756,6 @@ class ObjectMgr
         void LoadSpellTemplate();
         void LoadCreatureTemplateSpells();
 
-        void LoadWeatherZoneChances();
         void LoadGameTele();
 
         void LoadNpcGossips();
@@ -835,15 +820,6 @@ class ObjectMgr
                     return &*set_itr;
 
             return NULL;
-        }
-
-        WeatherZoneChances const* GetWeatherChances(uint32 zone_id) const
-        {
-            WeatherZoneMap::const_iterator itr = mWeatherZoneMap.find(zone_id);
-            if (itr != mWeatherZoneMap.end())
-                return &itr->second;
-            else
-                return NULL;
         }
 
         CreatureDataPair const* GetCreatureDataPair(uint32 guid) const
@@ -1223,8 +1199,6 @@ class ObjectMgr
 
         QuestPOIMap         mQuestPOIMap;
         QuestPhaseMapsMap   mQuestPhaseMap;
-
-        WeatherZoneMap      mWeatherZoneMap;
 
         // character reserved names
         typedef std::set<std::wstring> ReservedNamesMap;
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "13017"
+ #define REVISION_NR "13018"
 #endif // __REVISION_NR_H__
