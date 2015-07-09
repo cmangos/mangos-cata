c0dd72f64d040602563d8cd9f84282fb42997e60
stfx <stfx@hotmail.de>
2015-07-09 15:40:46 +0200

    [c13076] Fix missing semicolon in sql update file
    Thanks to @RHOPKINS13 and @evil-at-wow
    
    (based on commit [12863] - 2f55296)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- sql/updates/c12868_01_mangos_spell_template.sql
+++ sql/updates/c12868_01_mangos_spell_template.sql
@@ -2,4 +2,4 @@ ALTER TABLE db_version CHANGE COLUMN required_c12856_01_mangos_creature_template
 
 DELETE FROM spell_template WHERE id=21883;
 INSERT INTO spell_template VALUES
-(21883, 0x00000180, 0x10000000, 0x00000000, 0x00000000, 0x00000000, 101, 225,  76,  22,    0,   0,   0, 178904,  0,    0,     'Summon Healed Celebrian Vine')
+(21883, 0x00000180, 0x10000000, 0x00000000, 0x00000000, 0x00000000, 101, 225,  76,  22,    0,   0,   0, 178904,  0,    0,     'Summon Healed Celebrian Vine');
