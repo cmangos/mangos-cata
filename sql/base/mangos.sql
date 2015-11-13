d82a2ae981031dadc019cc11bc2081beb42b02f9
xfurry <xfurry@scriptdev2.com>
2015-11-13 21:57:29 +0100

    Adding missing sd2 tables in mangos.sql
    (based on cmangos/mangos-wotlk@083e830)

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -2069,6 +2069,26 @@ INSERT INTO `hotfix_data` VALUES
 UNLOCK TABLES;
 
 --
+-- Table structure for table `gossip_texts`
+--
+
+DROP TABLE IF EXISTS `gossip_texts`;
+CREATE TABLE `gossip_texts` (
+  `entry` mediumint(8) NOT NULL,
+  `content_default` text NOT NULL,
+  `content_loc1` text,
+  `content_loc2` text,
+  `content_loc3` text,
+  `content_loc4` text,
+  `content_loc5` text,
+  `content_loc6` text,
+  `content_loc7` text,
+  `content_loc8` text,
+  `comment` text,
+  PRIMARY KEY (`entry`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Gossip Texts';
+
+--
 -- Dumping data for table `gossip_texts`
 --
 
