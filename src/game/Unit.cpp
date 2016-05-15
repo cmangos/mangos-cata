85972d7a0e5e9441f350ac9350922163e9daaa1c
Phatcat <holsthein@live.dk>
2016-05-15 14:49:07 +0200

    Unit logic
    - UpdateMeleeAttackingState : I guess I should study up some more on
    logical gates myself. thanks for pointing @VladimirMangos - return
    swingerror enstead of returning 0. if we wanted
    to return 0 we sould just return false.... ehh... why are we returning
    0? why is this functio not just void?
    
    - Attack : Logical error: If we are doing a mele attack and we are
    already attacking the unit, we should not return a false, but rather a
    true.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -718,7 +718,7 @@ bool Unit::UpdateMeleeAttackingState()
         player->SwingErrorMsg(swingError);
     }
 
-    return swingError == 0;
+    return swingError;
 }
 
 bool Unit::haveOffhandWeapon() const
@@ -6107,10 +6107,13 @@ bool Unit::Attack(Unit* victim, bool meleeAttack)
         if (m_attacking == victim)
         {
             // switch to melee attack from ranged/magic
-            if (meleeAttack && !hasUnitState(UNIT_STAT_MELEE_ATTACKING))
+            if (meleeAttack)
             {
-                addUnitState(UNIT_STAT_MELEE_ATTACKING);
-                SendMeleeAttackStart(victim);
+                if (!hasUnitState(UNIT_STAT_MELEE_ATTACKING))
+                {
+                    addUnitState(UNIT_STAT_MELEE_ATTACKING);
+                    SendMeleeAttackStart(victim);
+                }
                 return true;
             }
             return false;
