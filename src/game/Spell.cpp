e085cb972b86b81c7e48d3d3b1f491f1cbbf515c
Xfurry <xfurry@scriptdev2.com>
2016-05-21 17:15:06 +0200

    Add target exception for spell 30768 - Karazhan
    Partially fix cmangos/issues#825
    
    (based on cmangos/mangos-wotlk@8b4d22a)
    
    Signed-off-by: Xfurry <xfurry@cmangos.net>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1941,6 +1941,7 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
             switch (m_spellInfo->Id)
             {
                 // Do not target current victim
+                case 30769:                                 // Pick Red Riding Hood
                 case 30843:                                 // Enfeeble
                 case 31347:                                 // Doom
                 case 37676:                                 // Insidious Whisper
