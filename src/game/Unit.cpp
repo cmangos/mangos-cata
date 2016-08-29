5a73be2b8faa6b3a0f5aa7dec53cb93f781f2805
Cyberium <cybermomo@techemail.com>
2016-08-29 09:06:54 +0200

    Correctly apply Pet motion after possess

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12396,7 +12396,16 @@ void Unit::ResetControlState(bool attackCharmer /*= true*/)
             }
             else
             {
-                possessedCreature->GetMotionMaster()->MoveFollow(this, PET_FOLLOW_DIST, PET_FOLLOW_ANGLE);
+                // reset pet motion
+                if (possessedCreature->GetCharmInfo() && possessedCreature->GetCharmInfo()->HasCommandState(COMMAND_FOLLOW))
+                {
+                    possessedCreature->GetMotionMaster()->MoveFollow(this, PET_FOLLOW_DIST, PET_FOLLOW_ANGLE);
+                }
+                else
+                {
+                    possessedCreature->GetMotionMaster()->Clear(false);
+                    possessedCreature->GetMotionMaster()->MoveIdle();
+                }
             }
         }
         else
