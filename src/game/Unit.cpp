7a001d2a0687704e1a97991fc83f8450eccc976c
Cyberium <cybermomo@techemail.com>
2016-09-07 11:40:19 +0200

    Dont remove threat list when a creature is possessed. We have to save it instead to restore it later.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12254,7 +12254,7 @@ Unit* Unit::TakePossessOf(SpellEntry const* spellEntry, SummonPropertiesEntry co
     }
 
     // set temp possess ai (creature will not be able to react by itself)
-    pCreature->SetPossessed(true);
+    pCreature->SetPossessed();
     
     if (player)
     {
@@ -12278,9 +12278,9 @@ bool Unit::TakePossessOf(Unit* possessed)
     if (GetTypeId() == TYPEID_PLAYER)
         player = static_cast<Player *>(this);
 
-    possessed->CombatStop(true);
-    possessed->DeleteThreatList();
-    possessed->getHostileRefManager().deleteReferences();
+    // stop combat but keep threat list
+    possessed->AttackStop(true, true);
+    possessed->ClearInCombat();
 
     possessed->addUnitState(UNIT_STAT_CONTROLLED);
     possessed->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED);
@@ -12293,8 +12293,9 @@ bool Unit::TakePossessOf(Unit* possessed)
     if (possessed->GetTypeId() == TYPEID_UNIT)
     {
         possessedCreature = static_cast<Creature *>(possessed);
-        possessedCreature->SetPossessed(true);
+        possessedCreature->SetPossessed();
         possessedCreature->GetMotionMaster()->Clear(true, true);
+        possessedCreature->StopMoving(true);
         possessedCreature->SetFactionTemporary(getFaction(), TEMPFACTION_NONE);
         possessedCreature->SetWalk(IsWalking(), true);
     }
@@ -12351,11 +12352,7 @@ void Unit::ResetControlState(bool attackCharmer /*= true*/)
         }
         return;
     }
-
-    possessed->CombatStop(true);
-    possessed->DeleteThreatList();
-    possessed->getHostileRefManager().deleteReferences();
-
+    
     possessed->clearUnitState(UNIT_STAT_CONTROLLED);
     possessed->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED);
     possessed->SetCharmerGuid(ObjectGuid());
@@ -12365,7 +12362,8 @@ void Unit::ResetControlState(bool attackCharmer /*= true*/)
     if (possessed->GetTypeId() == TYPEID_UNIT)
     {
         possessedCreature = static_cast<Creature *>(possessed);
-        possessedCreature->SetPossessed(false);
+        possessedCreature->ClearTemporaryFaction();
+        possessedCreature->SetPossessed(false, this);
     }
 
     if (player)
@@ -12410,12 +12408,6 @@ void Unit::ResetControlState(bool attackCharmer /*= true*/)
         {
             if (player)
                 player->RemovePetActionBar();
-
-            CreatureInfo const* cinfo = possessedCreature->GetCreatureInfo();
-            possessedCreature->ClearTemporaryFaction();
-            //TODO: find the correct rule to attack back the controller
-            /*if (attackCharmer)
-                possessedCreature->AddThreat(this, possessedCreature->GetHealth());*/
         }
     }
 }
