1a1003c5dee7aa7bd7ae0ddee4aec4e03bba923a
Cala <calaftp@free.fr>
2016-12-13 23:50:14 +0100

    Update revision_sql.h
    Forgot this one. Sorry.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_c12484_02_realmd_account_access"
  #define REVISION_DB_CHARACTERS "required_12942_01_characters_reset_talents"
- #define REVISION_DB_MANGOS "required_12944_01_mangos_item_extraflags"
+ #define REVISION_DB_MANGOS "required_12947_01_mangos_game_event"
 #endif // __REVISION_SQL_H__
