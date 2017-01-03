6480363bcfdf8f3514a7119e8b574bf4f3589efc
killerwife <killerwife@gmail.com>
2017-01-03 22:01:49 +0100

    [12950] Add ExtraFlags and customData1 to GO template
    Needs SQL:
    
    UPDATE gameobject_template SET ExtraFlags=1 WHERE displayId IN (4392,4472,4491,6785,3073,7998);
    
    Also
    UPDATE gameobject_template SET CustomData1=1 WHERE entry=103661;
    UPDATE gameobject_template SET CustomData1=2 WHERE entry IN(180057,187446);
    This fixes bubbly fissure GOs not triggering on friendlies + Items of Power GO not triggering on player near.
    
    Should fix issues like https://github.com/unified-db/Database/commit/5269a789481bfcae631fc822017109c5d7c562d3#commitcomment-19259234.
    Is needed in more places, but generally provides tools to tweak GOs more.

--- src/game/GameObject.h
+++ src/game/GameObject.h
@@ -42,6 +42,7 @@ struct GameObjectInfo
     char*   unk1;
     uint32  faction;
     uint32  flags;
+    uint32  ExtraFlags;
     float   size;
     uint32  questItems[6];
     union                                                   // different GO types have different data field
@@ -414,7 +415,22 @@ struct GameObjectInfo
         } raw;
     };
 
+    union
+    {
+        //6 GAMEOBJECT_TYPE_TRAP
+        struct
+        {
+            uint32 triggerOn;
+        } trapCustom;
+
+        struct
+        {
+            uint32 data[1];
+        } rawCustom;
+    };
+
     uint32 unk2;
+
     uint32 MinMoneyLoot;
     uint32 MaxMoneyLoot;
     uint32 ScriptId;
@@ -619,6 +635,11 @@ enum CapturePointSliderValue
     CAPTURE_SLIDER_ALLIANCE         = 100,                  // full alliance
     CAPTURE_SLIDER_HORDE            = 0,                    // full horde
     CAPTURE_SLIDER_MIDDLE           = 50                    // middle
+};
+
+enum GameobjectExtraFlags
+{
+    GAMEOBJECT_EXTRA_FLAG_CUSTOM_ANIM_ON_USE = 0x00000001,    // GO that plays custom animation on usage
 };
 
 class Unit;
