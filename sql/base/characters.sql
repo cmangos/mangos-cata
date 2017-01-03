bd33552f4b3b1960849c7bcb7a8a1fe66302e211
Ono <warlockbugs@users.noreply.github.com>
2017-01-03 20:33:01 +0300

    [12949] Cleanup mangle ranks and reset talents for druids
    Resolves issue with disappearing mangle ranks for existing characters, core is able to sort it out after talent reset automatically.

--- sql/base/characters.sql
+++ sql/base/characters.sql
@@ -21,8 +21,8 @@
 
 DROP TABLE IF EXISTS `character_db_version`;
 CREATE TABLE `character_db_version` (
-  `required_12942_01_characters_reset_talents` bit(1) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Last applied sql update to DB';
+  `required_12949_01_characters_mangle_cleanup` bit(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Last applied sql update to DB';
 
 --
 -- Dumping data for table `character_db_version`
