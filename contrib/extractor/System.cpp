c53a8b92bb012234e9b7b0a00be9435596ced154
Ono <warlockbugs@users.noreply.github.com>
2018-01-02 23:30:45 +0100

    Experimental MinGW support preparation: extractors and tools
    Fix types
    More strict windows preprocessor definitions
    
    (based on cmangos/mangos-wotlk@f9924c35a)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- contrib/extractor/System.cpp
+++ contrib/extractor/System.cpp
@@ -6,7 +6,7 @@
 #include <set>
 #include <cstdlib>
 
-#ifdef WIN32
+#ifdef _WIN32
 #include "direct.h"
 #include <windows.h>
 #else
@@ -20,7 +20,7 @@
 #include "loadlib/wdt.h"
 #include <fcntl.h>
 
-#ifndef WIN32
+#ifndef _WIN32
 #include <unistd.h>
 #endif
 
@@ -86,7 +86,7 @@ static char const* langs[] = {"enGB", "enUS", "deDE", "esES", "frFR", "koKR", "z
 
 void CreateDir(const std::string& Path)
 {
-#ifdef WIN32
+#ifdef _WIN32
     _mkdir(Path.c_str());
 #else
     mkdir(Path.c_str(), 0777);
--- contrib/vmap_extractor/vmapextract/adtfile.cpp
+++ contrib/vmap_extractor/vmapextract/adtfile.cpp
@@ -24,7 +24,7 @@
 #include <algorithm>
 #include <cstdio>
 
-#ifdef WIN32
+#ifdef _WIN32
 #define snprintf _snprintf
 #endif
 
