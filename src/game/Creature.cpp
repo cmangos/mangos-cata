10e5923f0a56516138e373999b4e47cebf6e1c9c
killerwife <killerwife@gmail.com>
2018-01-02 23:58:45 +0100

    Interrupt moving before corpse is removed to avoid spline malfunction
    (based on cmangos/mangos-wotlk@8f8ecc9c7)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -223,6 +223,8 @@ void Creature::RemoveCorpse(bool inPlace)
     if (respawnDelay)
         m_respawnTime = time(nullptr) + respawnDelay;
 
+    InterruptMoving();
+
     float x, y, z, o;
     GetRespawnCoord(x, y, z, &o);
     GetMap()->CreatureRelocation(this, x, y, z, o);
