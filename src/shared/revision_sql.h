111c3bbc4a1da41757261fb114cf996dd37d3bf7
Cala <calaftp@free.fr>
2018-01-04 00:20:47 +0100

    Playerbot: move SQL updates into core
    (based on cmangos/mangos-wotlk@378456487)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_c12484_02_realmd_account_access"
- #define REVISION_DB_CHARACTERS "required_12949_01_characters_mangle_cleanup"
- #define REVISION_DB_MANGOS "required_13954_02_mangos_gameobjects"
+ #define REVISION_DB_CHARACTERS "required_13957_03_characters_playerbot_quest_data"
+ #define REVISION_DB_MANGOS "required_13957_01_mangos_gossip_menu_option"
 #endif // __REVISION_SQL_H__
