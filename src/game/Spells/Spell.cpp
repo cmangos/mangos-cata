3d8cff3ff73b0f1e8953fe09bad874644d20e1b2
killerwife <killerwife@gmail.com>
2018-01-03 23:55:57 +0100

    Add persistent value for spell scripting
    (based on cmangos/mangos-wotlk@d43ffcf30)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -447,6 +447,8 @@ Spell::Spell(Unit* caster, SpellEntry const* info, uint32 triggeredFlags, Object
 
     m_affectedTargetCount = GetAffectedTargets(m_spellInfo, caster);
 
+    m_scriptValue = 0;
+
     CleanupTargetList();
 }
 
