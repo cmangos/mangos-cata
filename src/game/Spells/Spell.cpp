df5051ddc9056a017e242379835b94ab23aa2260
killerwife <killerwife@gmail.com>
2018-01-05 00:31:28 +0100

    Never seem to send this on smsg_spell_start
    (based on cmangos/mangos-wotlk@1c894d7e3)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -4195,9 +4195,6 @@ void Spell::SendSpellStart()
     if (HasPersistentAuraEffect(m_spellInfo))
         castFlags |= CAST_FLAG_PERSISTENT_AA;
 
-    if (m_CastItem)
-        castFlags |= CAST_FLAG_UNKNOWN7;
-
     if (m_spellInfo->runeCostID)
         castFlags |= CAST_FLAG_UNKNOWN19;
 
