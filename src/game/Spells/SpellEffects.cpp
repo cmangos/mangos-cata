195edf6d68ac5268ef911cd972ed15663631f52d
Ono <warlockbugs@users.noreply.github.com>
2018-01-06 14:39:43 +0100

    Implemented unified PVP Sanctuary API
    The logic matches what you can find by calling lua UnitIsPVPSanctuary() in client for the expansion.
    
    API is unified for expansions it applies to.
    
    WotLK-specific changes:
    * Implemented UNIT_BYTE2_FLAG_SANCTUARY
    * Implemented sanctuary flag propogation for all controlled units
    
    (based on cmangos/mangos-wotlk@8b4d579aa)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -5887,7 +5887,10 @@ bool Spell::DoSummonCritter(CreatureSummonPositions& list, SummonPropertiesEntry
     if (m_caster->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED))
         critter->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED);
 
-    critter->SetByteValue(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_SUPPORTABLE | UNIT_BYTE2_FLAG_UNK5);
+    if (m_caster->IsPvPSanctuary())
+        critter->SetPvPSanctuary(true);
+
+    critter->SetByteFlag(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_UNK5);
 
     return true;
 }
@@ -5966,6 +5969,9 @@ bool Spell::DoSummonGuardian(CreatureSummonPositions& list, SummonPropertiesEntr
         if (m_caster->IsPvP())
             spawnCreature->SetPvP(true);
 
+        if (m_caster->IsPvPSanctuary())
+            spawnCreature->SetPvPSanctuary(true);
+
         if (CharmInfo* charmInfo = spawnCreature->GetCharmInfo())
         {
             charmInfo->SetPetNumber(pet_number, false);
@@ -5980,7 +5986,7 @@ bool Spell::DoSummonGuardian(CreatureSummonPositions& list, SummonPropertiesEntr
 
         m_caster->AddGuardian(spawnCreature);
 
-        spawnCreature->SetByteValue(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_SUPPORTABLE | UNIT_BYTE2_FLAG_UNK5);
+        spawnCreature->SetByteFlag(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_UNK5);
     }
 
     return true;
@@ -6046,6 +6052,9 @@ bool Spell::DoSummonTotem(SpellEffectEntry const* effect, uint8 slot_dbc)
 
     if (m_caster->IsFFAPvP())
         pTotem->SetFFAPvP(true);
+
+    if (m_caster->IsPvPSanctuary())
+        pTotem->SetPvPSanctuary(true);
 
     // sending SMSG_TOTEM_CREATED before add to map (done in Summon)
     if (slot < MAX_TOTEM_SLOT && m_caster->GetTypeId() == TYPEID_PLAYER)
@@ -6767,6 +6776,9 @@ void Spell::EffectTameCreature(SpellEffectEntry const* /*effect*/)
     if (plr->IsFFAPvP())
         pet->SetFFAPvP(true);
 
+    if (plr->IsPvPSanctuary())
+        pet->SetPvPSanctuary(true);
+
     pet->GetCharmInfo()->SetPetNumber(sObjectMgr.GeneratePetNumber(), true);
 
     pet->GetCharmInfo()->SetReactState(REACT_DEFENSIVE);
@@ -6898,7 +6910,7 @@ void Spell::EffectSummonPet(SpellEffectEntry const* effect)
     {
         NewSummon->SetUInt32Value(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_NONE);
 
-        NewSummon->SetByteValue(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_SUPPORTABLE | UNIT_BYTE2_FLAG_AURAS);
+        NewSummon->SetByteValue(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_AURAS);
 
         NewSummon->GetCharmInfo()->SetPetNumber(pet_number, true);
 
@@ -6910,6 +6922,9 @@ void Spell::EffectSummonPet(SpellEffectEntry const* effect)
 
         if (m_caster->IsFFAPvP())
             NewSummon->SetFFAPvP(true);
+
+        if (m_caster->IsPvPSanctuary())
+            NewSummon->SetPvPSanctuary(true);
 
         NewSummon->SavePetToDB(PET_SAVE_AS_CURRENT);
         ((Player*)m_caster)->PetSpellInitialize();
@@ -12157,6 +12172,9 @@ void Spell::EffectCreateTamedPet(SpellEffectEntry const* effect)
 
     if (unitTarget->IsFFAPvP())
         newTamedPet->SetFFAPvP(true);
+
+    if (unitTarget->IsPvPSanctuary())
+        newTamedPet->SetPvPSanctuary(true);
 
     newTamedPet->InitStatsForLevel(unitTarget->getLevel());
     newTamedPet->InitPetCreateSpells();
