a738009544aea66d19af2587c591d905cfe77152
killerwife <killerwife@gmail.com>
2018-01-06 21:22:53 +0100

    Add questgiver_greeting and locales to properly handle SMSG_QUESTGIVER_QUEST_LIST
    TODO: Remove npc_gossip
    
    (based on cmangos/mangos-wotlk@40ae84a4a)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/Chat/Chat.h
+++ src/game/Chat/Chat.h
@@ -453,6 +453,7 @@ class ChatHandler
         bool HandleReloadGameGraveyardZoneCommand(char* args);
         bool HandleReloadGameTeleCommand(char* args);
         bool HandleReloadGossipMenuCommand(char* args);
+        bool HandleReloadQuestgiverGreetingCommand(char* args);
         bool HandleReloadGOQuestRelationsCommand(char* args);
         bool HandleReloadGOQuestInvRelationsCommand(char* args);
         bool HandleReloadHotfixDataCommand(char* args);
@@ -468,6 +469,7 @@ class ChatHandler
         bool HandleReloadLocalesPageTextCommand(char* args);
         bool HandleReloadLocalesPointsOfInterestCommand(char* args);
         bool HandleReloadLocalesQuestCommand(char* args);
+        bool HandleReloadQuestgiverGreetingLocalesCommand(char* args);
         bool HandleReloadLootTemplatesCreatureCommand(char* args);
         bool HandleReloadLootTemplatesDisenchantCommand(char* args);
         bool HandleReloadLootTemplatesFishingCommand(char* args);
